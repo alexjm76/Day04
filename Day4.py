1bd1843c08ab01ed877c98076ebf6af40bdca8f8
Jungmoo Yoon <alexjm@naver.com>
2023-01-17 09:36:19 +0900

    tuple base

---
+++ Day4.py
@@ -0,0 +1,14 @@
+#tuple
+
+a="harry",
+b=("harry",)
+c=() #empty tuple
+d="harry","ron"  #2개 이상일때는 마지막에 ,안붙여도 됨,
+e=("hermione")  #string
+f = ("harry","ron")
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
