64fd6dd0e4bea2a7bf181ace4fa56188c1560dba
Jungmoo Yoon <alexjm@naver.com>
2023-01-17 10:26:39 +0900

    tuple to list to tuple

--- Day4.py
+++ Day4.py
@@ -1,14 +1,12 @@
-#tuple
+#list
 
-a="harry",
-b=("harry",)
-c=() #empty tuple
-d="harry","ron"  #2개 이상일때는 마지막에 ,안붙여도 됨,
-e=("hermione")  #string
-f = ("harry","ron")
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
+scores =("B+","A+","C+")
+# print(scores[1])
+# scores[1]="C+"
+# scores["A+"] #tupel is immutable 변하지 않는다
+
+temp = list(scores)
+temp[1]="C+"
+temp[2]="A+"
+cores = tuple(temp)
+print(scores[1],scores[2])
