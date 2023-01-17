ecc84fea16a354540d40d0ea7025da4ad54901ad
Jungmoo Yoon <alexjm@naver.com>
2023-01-17 14:23:03 +0900

    dictionary basic

--- Day4.py
+++ Day4.py
@@ -1,12 +1,14 @@
-#7.4
-things = ["mozzarella" , "ciderella", "salmonella"]
+#dictionary
 
-print(things[1].title())
+students = {"name" : "윤정무", "age": 27, "eyes": [0.9, 1.1]}
 
-things[0] = things[0].upper()
+#for k in students:  #students.keys를 생략해서 students라고 표현
+#    print(k)
+#
+# for k in students.values():  #value 값만 출력
+#     print(k)
 
-print(things)
-
-print(f"{things.pop()}")
-print(things)
-
+for k , v in students.items():
+    print(f"{k} : {v}")
+print(f"이름은 {students['name']},나이는 {students['age']}세", end=' ')
+print(f"시련은 좌 : {students['eyes'][0]}, 우 : {students['eyes'][1]}")
